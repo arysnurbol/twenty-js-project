404c86446265be0a3b5e00443c874307c034dfc3
nurbol <nurbol.amit@hardcode.kz>
2023-08-21 07:20:38 +0600

    custom video player

---
+++ custom-video-player/script.js
@@ -0,0 +1,65 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+
+// Event listener
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
+
+// Play & pause video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// updata play/pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+    }
+}
+
+// Update progress
+function updateProgress() {
+     progress.value = (video.currentTime / video.duration) * 100;
+
+     // Get minutes
+     let mins = Math.floor(video.currentTime / 60);
+     if (mins < 10) {
+        mins = '0' + String(mins);
+     }
+
+     // Get seconds
+     let s = Math.floor(video.currentTime % 60);
+     if (s < 10) {
+        s = '0' + String(s);
+     }
+
+     timestamp.innerHTML = `${mins}:${s}`; 
+}
+
+// Set video time to progress
+function setVideoProgress() {
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+// Stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
